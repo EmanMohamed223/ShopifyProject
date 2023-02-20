158d9765646ee4852ba912e9c051bcece735c9b5
asmaa <asmaaabdelfattah491@yahoo.com>
2023-02-20 11:37:12 +0200

    Home Screen Design

---
+++ ShopifyProj/ViewController/HomeController/BrandCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  BrandCollectionViewCell.swift
+//  ShopifyProj
+//
+//  Created by Asmaa_Abdelfattah on 30/11/1401 AP.
+//
+
+import UIKit
+
+class BrandCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var brandTitle: UILabel!
+    @IBOutlet weak var brandImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
